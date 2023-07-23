8a2320bd2d0610751b82f5d0aaa6fb799305fc83
Ramakanth <ramakanthvattipally@gmail.com>
2023-07-23 17:06:23 +0100

    overrite

---
+++ scripts.ps1
@@ -0,0 +1,12 @@
+{
+    "clientId": "62522b65-1fea-4279-9ca1-68e589aebfd6",
+    "clientSecret": "eyd8Q~4xcIJ4E~0RFoIOKGdZXq1Mn1YZBB9TFbgY",
+    "subscriptionId": "3fc031f6-75e6-4ac2-a2f4-671def6fb32e",
+    "tenantId": "015dcbaf-7501-4b28-a8b4-732a9b4b4172",
+    "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+    "resourceManagerEndpointUrl": "https://management.azure.com/",
+    "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+    "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+    "galleryEndpointUrl": "https://gallery.azure.com/",
+    "managementEndpointUrl": "https://management.core.windows.net/"
+  }
